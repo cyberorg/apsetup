3029e00c7a9d0f3c7023654b390b8900bd0a2e13
Jigish Gohil <cyberorg@cyberorg.info>
2020-05-07 10:40:51 +0530

    fix masquerade and progress bar

--- src/var/www/html/ap/apsetup.php
+++ src/var/www/html/ap/apsetup.php
@@ -1,3 +1,15 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Akobox Access Point</title>
+<link href="form.css" rel="stylesheet">
+</head>
+<body>
+<div id="main">
+<div id="first">
+	<center>
+<h2>Setup Akobox Access Point Security</h2>
+</center>
 <?php
 $AP_SSID = $_POST['AP_SSID'];
 $AP_PASS = $_POST['AP_PASS'];
@@ -11,12 +23,33 @@ echo "<center><br><br><b>Passphrase:</b>&nbsp;";
 echo $AP_PASS."</center>";
 echo "<center><br><br><b>IP Address:</b>&nbsp;";
 echo $AP_IP."</center>";
-echo "<center><br><br><b>Gateway:</b>&nbsp;";
+echo "<center><br><br><b>Allow internet:</b>&nbsp;";
 echo $NET_CONNECT."</center>";
 echo "<br><br><center><b>Please reconnect your devices to this access point</b></center>";
 
 $cmd = "/usr/bin/apsetup $AP_SSID $AP_PASS $AP_IP $NET_CONNECT";
 exec($cmd);
-header("refresh:50; url=http://$AP_IP/");
+header("refresh:40; url=http://$AP_IP/");
 ?>
-
+<script type="text/javascript">
+var timeleft = 40;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+  }
+  document.getElementById("progressBar").value = 40 - timeleft;
+  timeleft -= 1;
+}, 1000);
+</script>
+<br>
+<br>
+<center>
+<h4>Activating your new settings</h4>
+<progress value="0" max="40" id="progressBar"></progress>
+</center>
+<br>
+<br>
+</div>
+</div>
+</body>
+</html>
--- src/var/www/html/ap/index.php
+++ src/var/www/html/ap/index.php
@@ -14,23 +14,25 @@
 <form method="post" action="apsetup.php">
 <?php include('apconfread.php');?>
 <br>
+
 <h4>Enter Akobox WiFi network  name (SSID)</h4>
-<center>	<input type="text" name="AP_SSID" value="<?php echo $ar['AP_SSID']; ?>" required><br></center>
+<center><input type="text" name="AP_SSID" value="<?php echo $ar['AP_SSID']; ?>" required></center>
 
 <h4>Enter passphrase (8 to 20 alphanumeric characters)</h4>
-<center>	<input type="text" name="AP_PASS" value="<?php echo $ar['AP_PASS']; ?>" pattern="^[A-Za-z0-9]{8,20}$" required><br></center>
+<center><input type="text" name="AP_PASS" value="<?php echo $ar['AP_PASS']; ?>" pattern="^[A-Za-z0-9]{8,20}$" required></center>
+
 <h4>Enter IP Address, e.g. 192.168.4.1</h4>
-<center>	<input type="text" name="AP_IP" value="<?php echo $ar['AP_IP']; ?>" pattern="^(?:(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])(\.(?!$)|$)){4}$" required><br></center>
+<center><input type="text" name="AP_IP" value="<?php echo $ar['AP_IP']; ?>" pattern="^(?:(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])(\.(?!$)|$)){4}$" required></center>
+
 <h4>Enable/Disable internet access through this Access Point</h4>
 <center>  <select id="NET_CONNECT" name="NET_CONNECT">
     <option value="true" <?php if ($ar['NET_CONNECT']==true) echo 'selected="selected"';?>>Enable</option>
     <option value="false" <?php if ($ar['NET_CONNECT']==false) echo 'selected="selected"';?>>Disable</option>
-  </select><br></center>
-
+  </select></center>
 <br> 
 <br> 
 <center><input type="submit" value="Submit" /></center>
-<br />
+<br>
 </form>
 </div>
 </div>
